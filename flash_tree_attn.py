c0b06f6c5c0a5611bb0d309d4a679ff4162ae81c
Cunxiao Du <cnsdunm@gmail.com>
2024-12-24 15:17:05 +0800

    Update flash_tree_attn.py

--- flash_tree_attn.py
+++ flash_tree_attn.py
@@ -1,6 +1,6 @@
 # Applying Flash-Decoding as descibed in
 # https://pytorch.org/blog/flash-decoding/
-# by Tri Dao, 2023、
+# by Tri Dao, 2023
 
 # This file has been modified from its original version.
 # The original file can be found at: https://github.com/hpcaitech/ColossalAI/blob/feat/speculative-decoding/colossalai/kernel/triton/flash_decoding.py
@@ -18,8 +18,11 @@ def _flash_decoding_fwd_kernel(
     VCache,  # [num_blocks, num_kv_heads, block_size, head_dim]
     mid_o,  # [batch_size * q_len, head_num, kv_split_num, head_dim]
     mid_o_lse,  # [batch_size * q_len, head_num, kv_split_num]
+    maskone, #[batch_size, q_len, mask_kv_len]
+    masktwo, #[batch_size, q_len, mask_kv_len]
     kv_seq_len,  # [batch_size]
     q_len: tl.constexpr,
+    mask_kv_len: tl.constexpr,
     batch_size,
     sm_scale,
     stride_qt,
@@ -42,6 +45,12 @@ def _flash_decoding_fwd_kernel(
     stride_mid_o_lset,
     stride_mid_o_lseh,
     stride_mid_o_lseb,
+    stride_s_b,
+    stride_s_qlen,
+    stride_s_kvlen,
+    stride_s_two_b,
+    stride_s_two_qlen,
+    stride_s_two_kvlen,
     KV_GROUPS: tl.constexpr,
     BLOCK_KV: tl.constexpr,
     HEAD_DIM: tl.constexpr,
@@ -93,15 +102,29 @@ def _flash_decoding_fwd_kernel(
     k_cur_block = tl.load(K_block_ptr)
     v_cur_block = tl.load(V_block_ptr)
 
+    cur_s_offset = cur_token_idx * stride_s_b
+    S_block_ptr_1 = tl.make_block_ptr(
+        base=maskone + cur_s_offset,
+        shape=(q_len, mask_kv_len),
+        strides=(stride_s_qlen, stride_s_kvlen),
+        offsets=(0, 0),
+        block_shape=(q_len, mask_kv_len),
+        order=(0, 1),
+    )
+
+    cur_stwo_offset = cur_token_idx * stride_s_two_b
+    S_block_ptr_2 = tl.make_block_ptr(
+        base=masktwo + cur_stwo_offset,
+        shape=(q_len, mask_kv_len),
+        strides=(stride_s_two_qlen, stride_s_two_kvlen),
+        offsets=(0, 0),
+        block_shape=(q_len, mask_kv_len),
+        order=(0, 1),
+    )  
+
     acc = tl.zeros([q_len, HEAD_DIM], dtype=tl.float32)
     # use block size of the paged/blocked kv cache
     S_ij = tl.zeros([q_len, BLOCK_KV], dtype=tl.float32)
-
-    # print(f"grid: ({(tl.program_id(0), tl.program_id(1), tl.program_id(2))})")
-    # print(f"q is {q}, sum is {tl.sum(q)}")
-    # print(f"k is {k_cur_block}, sum is {tl.sum(k_cur_block)}")
-    # print(f"v is {v_cur_block}, sum is {tl.sum(v_cur_block)}")
-    # print('--------')
 
     # NOTE a trick to come across triton's requirement that values in both first and second input shapes must be >= 16,
     # Multiplying two tensors with shapes [1, d] * [d, block_size] will fail.
@@ -110,8 +133,22 @@ def _flash_decoding_fwd_kernel(
     S_ij += tl.dot(q, k_cur_block)
     S_ij = tl.where(block_mask[None, :], S_ij, float("-inf"))
     S_ij *= sm_scale
+
     m = tl.max(S_ij, 1)
     S_ij -= m[:, None]
+
+    # apply tree mask
+    total_blocks = (cur_kv_seq_len + BLOCK_KV - 1) // BLOCK_KV
+
+    if block_start_kv == total_blocks - 2:
+        # last two
+        mask = tl.load(S_block_ptr_1)
+        S_ij = tl.where(mask, S_ij, float("-inf"))
+
+    elif block_start_kv == total_blocks - 1:
+        # last one
+        mask = tl.load(S_block_ptr_2)
+        S_ij = tl.where(mask, S_ij, float("-inf"))
     p_ij_hat = tl.exp(S_ij)
     l_i = tl.sum(p_ij_hat, 1)
     p_ij_hat = p_ij_hat.to(v_cur_block.type.element_ty)# q_len, block_kv
@@ -135,7 +172,6 @@ def _flash_decoding_fwd_kernel(
     )
     tl.store(offsets_mid_o, acc)
 
-    # BUG
     offsets_qlen = tl.arange(0, q_len)
     offsets_mid_o_lse = (
         cur_token_idx * stride_mid_o_lset + cur_head_idx * stride_mid_o_lseh + block_start_kv * stride_mid_o_lseb + offsets_qlen
@@ -219,7 +255,7 @@ def flash_decoding_attention(
     v_cache: torch.Tensor,
     current_k: torch.Tensor,
     current_v: torch.Tensor,
-    mask: torch.Tensor,
+    tree_mask: torch.Tensor,
     kv_seq_len: torch.Tensor,
     block_size: int = 64,
     max_seq_len_in_batch: int = None,
@@ -239,7 +275,7 @@ def flash_decoding_attention(
         v_cache (torch.Tensor): [bsz, num_kv_heads, ctx, head_dim]
         current_k: (torch.Tensor): [bsz, num_kv_heads, q_len, head_dim]
         current_v: (torch.Tensor): [bsz, num_kv_heads, q_len, head_dim]
-        mask: (torch.tensor): [bsz, num_heads, qlen, qlen] or [qlen, qlen]
+        tree_mask: (torch.tensor): [bsz, num_heads, q_len, kv_len], mask will apply to the last kv_len of cache
         kv_seq_len (torch.Tensor): [batch_size]
             records the (kv) sequence lengths incorporating past kv sequence lengths.
         max_seq_len_in_batch (int): Maximum sequence length in the batch.
@@ -263,6 +299,33 @@ def flash_decoding_attention(
     assert head_dim in {32, 64, 128, 256}
     assert k_cache.size(-2) == v_cache.size(-2), f"Got incompatible block size on kv caches:\n"
 
+    append_len = current_k.size(2)
+    insert_indices = kv_seq_len.unsqueeze(-1) + torch.arange(append_len, device=kv_seq_len.device).unsqueeze(0)
+    insert_indices = insert_indices[:, None, :, None].expand(-1, k_cache.size(1), -1, head_dim)
+
+    k_cache.scatter_(2, insert_indices, current_k)
+    v_cache.scatter_(2, insert_indices, current_v)
+
+    kv_seq_len += append_len
+
+    tree_mask_1 = tree_mask.new_ones((bsz, q_len, block_size))
+    tree_mask_2 = tree_mask.new_ones((bsz, q_len, block_size))
+
+    last_len = kv_seq_len % block_size
+    mask_kv_len = tree_mask.size(-1)
+
+    for i in range(bsz):
+        tail_index = min(mask_kv_len, last_len[i])
+        if mask_kv_len > last_len[i]:
+            tree_mask_1[i, :, block_size + last_len[i] - mask_kv_len:] = tree_mask[i, :, 0: mask_kv_len - last_len[i]]
+        
+            if tail_index != 0:
+                tree_mask_2[i, :, :tail_index] = tree_mask[i, :, -tail_index:]
+            else:
+                tree_mask_2[i, :, 0] = tree_mask[i, :, -1]
+        else:
+            tree_mask_2[i, :, last_len[i]-mask_kv_len: last_len[i]] = tree_mask[i]
+
     # NOTE BLOCK_KV could be considered as block splitting the sequence on k/v
     # For now, BLOCK_KV is supposed to be equivalent with the size of physical cache block (i.e.`block_size`)
     assert block_size in {16, 32, 64, 128}
@@ -273,15 +336,13 @@ def flash_decoding_attention(
     # For compatibility (TODO revise modeling in future)
     kv_max_split_num = (max_seq_len_in_batch + BLOCK_KV - 1) // BLOCK_KV
 
-    if mid_output is None:
-        mid_output = torch.empty(
-            (bsz, num_heads, kv_max_split_num, q_len, head_dim), dtype=torch.float32, device=q.device
-        )
-    if mid_output_lse is None:
-        mid_output_lse = torch.empty((bsz, num_heads, kv_max_split_num, q_len), dtype=torch.float32, device=q.device)
-    if output is None:
-        # A hack to prevent `view` operation in modeling
-        output = torch.empty((bsz, num_heads, q_len, head_dim), dtype=q.dtype, device=q.device)
+    mid_output = torch.empty(
+        (bsz, num_heads, kv_max_split_num, q_len, head_dim), dtype=torch.float32, device=q.device
+    )
+    mid_output_lse = torch.empty((bsz, num_heads, kv_max_split_num, q_len), dtype=torch.float32, device=q.device)
+    
+    output = torch.empty((bsz, num_heads, q_len, head_dim), dtype=q.dtype, device=q.device)
+    
     output_lse = torch.empty((bsz, num_heads, q_len, 1), dtype=q.dtype, device=q.device)
     assert (
         mid_output.size(2) == mid_output_lse.size(2) >= kv_max_split_num
@@ -297,15 +358,18 @@ def flash_decoding_attention(
         num_heads,
         triton.cdiv(triton.next_power_of_2(max_seq_len_in_batch), META["BLOCK_KV"]),
     )
-
+    
     _flash_decoding_fwd_kernel[grid](
         q,
         k_cache,
         v_cache,
         mid_output,
         mid_output_lse,
+        tree_mask_1,
+        tree_mask_2,
         kv_seq_len,
         q_len,
+        tree_mask_1.size(-1),
         bsz,
         sm_scale,
         q.stride(0),
@@ -328,6 +392,12 @@ def flash_decoding_attention(
         mid_output_lse.stride(0),
         mid_output_lse.stride(1),
         mid_output_lse.stride(2),
+        tree_mask_1.stride(0),
+        tree_mask_1.stride(1),
+        tree_mask_1.stride(2),
+        tree_mask_2.stride(0),
+        tree_mask_2.stride(1),
+        tree_mask_2.stride(2),
         KV_GROUPS=kv_group_num,
         BLOCK_KV=block_size,
         HEAD_DIM=head_dim,
@@ -336,6 +406,9 @@ def flash_decoding_attention(
     # assert torch.allclose(mid_output[:, :, 0] , mid_output[:, :, 1], atol=1e-2, rtol=0)
     # assert torch.allclose(mid_output_lse[:, :, 0] , mid_output_lse[:, :, 1], atol=1e-2, rtol=0)
     # mid_output_lse = torch.permute(mid_output_lse, (0, 1, 3, 2)).contiguous()
+    print("output is nan: ", mid_output.isnan().any())
+    print("output lse is nan: ", mid_output_lse.isnan().any())
+
     grid = (triton.next_power_of_2(bsz), num_heads, q_len)
     _flash_decoding_fwd_reduce_kernel[grid](
         mid_output,
@@ -364,16 +437,7 @@ def flash_decoding_attention(
         HEAD_DIM=head_dim,
     )
 
-    p = torch.matmul(q, current_k.transpose(2, 3)) * sm_scale
-    p += mask
-    current_lse = p.logsumexp(dim=-1, keepdim=True)
-    p = torch.softmax(p.float(), dim=-1).half()
-    current_out = torch.matmul(p, current_v)
-
-    out = (output - F.sigmoid(current_lse - output_lse) * (output - current_out))
-
-
-    return out
+    return output
 
 import pytest
 
@@ -387,9 +451,14 @@ def torch_vanilla_attention(q, k, v, sm_scale):
 
 def torch_tree_attention(q, k, v, tree_mask, sm_scale):
     p = torch.matmul(q, k.transpose(2, 3)) * sm_scale
+    new_tree_mask = tree_mask.int()
+    float_tree_mask = tree_mask.float()
+    float_tree_mask[new_tree_mask.eq(0)] = float('-inf')
+    float_tree_mask[new_tree_mask.eq(1)] = 0
     mask = p.new_zeros(p.size())
-    mask[..., -tree_mask.size(0):, -tree_mask.size(1):] += tree_mask
+    mask[..., -tree_mask.size(0):, -tree_mask.size(1):] += float_tree_mask
     p += mask
+    # p[:, :, :, -32] = float('-inf')
     lse = p.logsumexp(dim=-1, keepdim=True)
     p = torch.softmax(p.float(), dim=-1).half()
     qk_ref_out = torch.matmul(p, v)
@@ -402,7 +471,7 @@ import torch.nn.functional as F
 
 def tree_mask(q):
     q_len = q.size(2)
-    mask = torch.zeros((q_len, q_len)).fill_(float('-inf'))
+    mask = torch.zeros((q_len, q_len))
     for i in range(q_len):
         for j in range(q_len):
             if i == j:
@@ -410,10 +479,21 @@ def tree_mask(q):
             else:
                 mask[i, j] = random.randint(0, 1)
     mask = mask.to(q.device)
-    mask[mask.eq(0)] = float('-inf')
     return mask
 
-@pytest.mark.parametrize("Z, H, N_CTX, HEAD_DIM, q_len", [(32, 32, 4096, 128, 128)])
+# Set the seed
+torch.manual_seed(42)
+
+# If using CUDA, also set the seed for CUDA
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(42)
+    torch.cuda.manual_seed_all(42)  # If using multi-GPU
+
+# Optional: Ensure deterministic behavior for cuDNN
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+@pytest.mark.parametrize("Z, H, N_CTX, HEAD_DIM, q_len", [(1, 32, 4096, 128, 32)])
 def test_tree_op(Z, H, N_CTX, HEAD_DIM, q_len):
     dtype=torch.float16
     # torch.manual_seed(20)
@@ -425,63 +505,69 @@ def test_tree_op(Z, H, N_CTX, HEAD_DIM, q_len):
     current_v = q.new_zeros(q.size()).normal_()
 
     mask = tree_mask(q)
-
+    mask[:, 0] = 1
     sm_scale = 1.0 / (HEAD_DIM**0.5)
+    kvtest = 100 # 353 wrong, 352 赋值wrong, 100 wrong
     # reference implementation
-    ref_out, _ = torch_tree_attention(q, torch.cat([k, current_k], dim=2), torch.cat([v, current_v], dim=2), mask, sm_scale)
-    
+    ref_out, _ = torch_tree_attention(q, torch.cat([k[:, :, :kvtest], current_k], dim=2), torch.cat([v[:, :, :kvtest], current_v], dim=2), mask.clone(), sm_scale)
+    mask = mask[None, :, :].repeat(Z, 1, 1)
+    mask = mask.int()
     # force right padding
-    kv_seq_len = torch.tensor([N_CTX] * Z).cuda()    
+    # 512 + 32
+    kv_seq_len = torch.tensor([kvtest] * Z).cuda()    
     out = flash_decoding_attention(q, k, v, current_k, current_v, mask, kv_seq_len, 128)
+    print((ref_out - out).sum(dim=-1))
+    print(ref_out.isnan().any())
+    print(out.isnan().any())
+    print((ref_out - out).sum(dim=-1).max())
     # current_out, current_lse = torch_tree_attention(q, current_k, current_v, mask, sm_scale)
 
     # tri_out = (cache_out - F.sigmoid(current_lse - cache_lse) * (cache_out - current_out))
 
-    assert  torch.allclose(ref_out, out, atol=1e-2, rtol=0)
-
-BATCH, N_HEADS, HEAD_DIM, Q_LEN = 2, 32, 128, 128
-# vary seq length for fixed head and batch=4
-configs = []
-# TODO: padding
-# TODO: KV Cache
-for Q_LEN in [16, 32, 64, 128]:
-    configs.append(
-        triton.testing.Benchmark(
-            x_names=["N_CTX"],
-            x_vals=[2**i for i in range(6, 9)],
-            line_arg="provider",
-            line_vals=["triton", "torch"],
-            line_names=["triton", "torch"],
-            styles=[("red", "-"), ("blue", "-"), ("green", "-")],
-            ylabel="ms",
-            plot_name=f"flash-attention-batch{BATCH}-head{N_HEADS}-d{HEAD_DIM}-qlen{Q_LEN}",
-            args={
-                "H": N_HEADS,
-                "Z": BATCH,
-                "HEAD_DIM": HEAD_DIM,
-                "q_len": Q_LEN,
-            },
-        ))
+    assert  torch.allclose(ref_out, out, atol=1e-3, rtol=0)
+# BATCH, N_HEADS, HEAD_DIM, Q_LEN = 2, 32, 128, 128
+# # vary seq length for fixed head and batch=4
+# configs = []
+# # TODO: padding
+# # TODO: KV Cache
+# for Q_LEN in [16, 32, 64, 128]:
+#     configs.append(
+#         triton.testing.Benchmark(
+#             x_names=["N_CTX"],
+#             x_vals=[2**i for i in range(6, 9)],
+#             line_arg="provider",
+#             line_vals=["triton", "torch"],
+#             line_names=["triton", "torch"],
+#             styles=[("red", "-"), ("blue", "-"), ("green", "-")],
+#             ylabel="ms",
+#             plot_name=f"flash-attention-batch{BATCH}-head{N_HEADS}-d{HEAD_DIM}-qlen{Q_LEN}",
+#             args={
+#                 "H": N_HEADS,
+#                 "Z": BATCH,
+#                 "HEAD_DIM": HEAD_DIM,
+#                 "q_len": Q_LEN,
+#             },
+#         ))
     
-@triton.testing.perf_report(configs)
-def benchmark(Z, H, N_CTX, HEAD_DIM, q_len, provider):
-    dtype=torch.float16
-    # torch.manual_seed(20)
-    q = (torch.empty((Z, H, q_len, HEAD_DIM), dtype=dtype, device="cuda").normal_(mean=0.0, std=0.5))
-    k = (torch.empty((Z, H, N_CTX + q_len, HEAD_DIM), dtype=dtype, device="cuda").normal_(mean=0.0, std=0.5))
-    v = (torch.empty((Z, H, N_CTX + q_len, HEAD_DIM), dtype=dtype, device="cuda").normal_(mean=0.0, std=0.5))
+# @triton.testing.perf_report(configs)
+# def benchmark(Z, H, N_CTX, HEAD_DIM, q_len, provider):
+#     dtype=torch.float16
+#     # torch.manual_seed(20)
+#     q = (torch.empty((Z, H, q_len, HEAD_DIM), dtype=dtype, device="cuda").normal_(mean=0.0, std=0.5))
+#     k = (torch.empty((Z, H, N_CTX + q_len, HEAD_DIM), dtype=dtype, device="cuda").normal_(mean=0.0, std=0.5))
+#     v = (torch.empty((Z, H, N_CTX + q_len, HEAD_DIM), dtype=dtype, device="cuda").normal_(mean=0.0, std=0.5))
     
-    mask = tree_mask(q)
-    kv_seq_len = torch.tensor([N_CTX] * Z).cuda()
-    blk=64
-    sm_scale = 1.0 / (HEAD_DIM**0.5)
+#     mask = tree_mask(q)
+#     kv_seq_len = torch.tensor([N_CTX] * Z).cuda()
+#     blk=64
+#     sm_scale = 1.0 / (HEAD_DIM**0.5)
 
-    quantiles = [0.5, 0.2, 0.8]
-    if provider == 'torch':
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch_tree_attention(q, k, v, mask, sm_scale), quantiles=quantiles)
-    if provider == 'triton':
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: flash_decoding_attention(q, k[:, :, :N_CTX], v[:, :, :N_CTX], k[:, :, N_CTX:], v[:, :, N_CTX:], mask, kv_seq_len, block_size=blk, sm_scale=sm_scale), quantiles=quantiles)
-    gbps = lambda ms: (q.numel() * q.element_size() + k.numel() * k.element_size() + v.numel() * v.element_size()) / ms * 1e-6
-    return ms, min_ms, max_ms #gbps(ms), gbps(max_ms), gbps(min_ms)
+#     quantiles = [0.5, 0.2, 0.8]
+#     if provider == 'torch':
+#         ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch_tree_attention(q, k, v, mask, sm_scale), quantiles=quantiles)
+#     if provider == 'triton':
+#         ms, min_ms, max_ms = triton.testing.do_bench(lambda: flash_decoding_attention(q, k[:, :, :N_CTX], v[:, :, :N_CTX], k[:, :, N_CTX:], v[:, :, N_CTX:], mask, kv_seq_len, block_size=blk, sm_scale=sm_scale), quantiles=quantiles)
+#     gbps = lambda ms: (q.numel() * q.element_size() + k.numel() * k.element_size() + v.numel() * v.element_size()) / ms * 1e-6
+#     return ms, min_ms, max_ms #gbps(ms), gbps(max_ms), gbps(min_ms)
 
-benchmark.run(print_data=True, show_plots=False)
+# benchmark.run(print_data=True, show_plots=False)
